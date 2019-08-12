14602b791d80e1beeeff72a348b7564a3111dba0
kensleyASC6 <kensley910@gmail.com>
2019-08-12 11:29:04 -0400

    changed a branch

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 let hello = "there";
 
 let general = "kenobi";
-=======
 let hello = "there"
 
 let judahsvar = "yeetosh"
->>>>>>> 310c21a58910ef25e3b470e2da607f37b908137a
+
+let branch = "change";
